050c014437ea3593d1eb6e187c1a5083b3b72ed0
ccatron88 <christophercatron@outlook.com>
2024-12-03 22:47:32 -0600

    Initial Commit

---
+++ Memory/MemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryApp.swift
+//  Memory
+//
+//  Created by Christopher Catron on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MemoryTests/MemoryTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryTests.swift
+//  MemoryTests
+//
+//  Created by Christopher Catron on 12/3/24.
+//
+
+import Testing
+@testable import Memory
+
+struct MemoryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
