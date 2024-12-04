4033dce91b9492889c9f9e401258e1b51436e7ff
ccatron88 <christophercatron@outlook.com>
2024-12-03 22:58:51 -0600

    Initial Commit CardView creation isFaceUp bool check

--- Memory/ContentView.swift
+++ Memory/ContentView.swift
@@ -2,20 +2,55 @@
 //  ContentView.swift
 //  Memory
 //
-//  Created by Christopher Catron on 12/3/24.
+//  Created by Christopher Catron on 11/30/24.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+            RoundedRectangle(cornerRadius: 12)
+            Text("Memory")
+                .font(.title)
+                .foregroundStyle(.black)
         }
-        .padding()
+        .foregroundStyle(.cyan)
+        .frame(width: 400, height: 80)
+        .padding(.vertical, 0)
+        
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
+            CardView()
+        }
+        .foregroundColor(.purple)
+        .padding(.horizontal, 2.0)
+        .padding(.vertical, 0)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        if isFaceUp {
+            ZStack {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                    .frame(width:50, height: 50)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                    .frame(width:50, height: 50)
+                Text("👻")
+            }
+        } else {
+            RoundedRectangle(cornerRadius: 12)
+                .frame(width:50, height: 50)
+        }
     }
 }
 
